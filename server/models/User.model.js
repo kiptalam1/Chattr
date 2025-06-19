3c6e66aa70a659cd5496611d368d3097b643a3b7
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-19 18:31:53 +0300

    fix: increase restrictions to user model.

---
+++ server/models/User.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			trim: true,
+			minlength: 2,
+			maxlength: 50,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 6,
+			select: false,
+		},
+		role: {
+			type: String,
+			enum: ["admin", "user"],
+			default: "user",
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export default mongoose.model("User", UserSchema);
