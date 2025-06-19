1cf1e159cb4fd416156a1e7b7295696c83f1ab1d
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-19 19:51:37 +0300

    feat: add registration routes.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+import authRoutes from "./routes/auth.route.js";
 
 // connect to database;
 mongoose
@@ -11,6 +12,14 @@ mongoose
 	.catch((error) => console.log("Error connecting to mongo DB !", error));
 
 const app = express();
+
+// middleware;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// routes;
+app.use("/api/v1/auth", authRoutes);
 
 // listen to app;
 const PORT = process.env.PORT;
