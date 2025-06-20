7fca72d12ed33b222938c58500074f61567c2905
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-20 20:28:38 +0300

    feat: add login functionality.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<LandingPage />} />
+				<Route path="/auth/login" element={<LoginPage />} />
+				<Route path="/auth/register" element={<RegisterPage />} />
 			</Routes>
 		</>
 	);
