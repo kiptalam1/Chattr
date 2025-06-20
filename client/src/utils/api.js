0d5e5d54cd0b751ded2d6b6619913dded4d4efa0
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-20 16:09:13 +0300

    fix: add interceptor for tokenbased auth.

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,14 @@ import axios from "axios";
 
 const API = axios.create({
 	baseURL: import.meta.env.VITE_API_BASE_URL,
-	withCredentials: true, // if you're using cookies
+});
+
+API.interceptors.request.use((config) => {
+	const token = localStorage.getItem("token");
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+	return config;
 });
 
 export default API;
