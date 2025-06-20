cc01b811f9d258ccb3e34bcb3cd409c11528c2ae
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-20 16:27:44 +0300

    feat: build authContext to manage JWT & user state.

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,40 @@
+// AuthContext to manage JWT & user state;
+import { useContext, createContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+	const [token, setToken] = useState(() => localStorage.getItem("token"));
+	const [user, setUser] = useState(() =>
+		JSON.parse(localStorage.getItem("token"))
+	);
+
+	const login = ({ token, user }) => {
+		setToken(token);
+		setUser(user);
+		localStorage.setItem("token", token);
+		localStorage.setItem("user", JSON.stringify(user));
+	};
+
+	const logout = () => {
+		setToken(null);
+		setUser(null);
+		localStorage.removeItem("token");
+		localStorage.removeItem("user");
+	};
+
+	return (
+		<AuthContext.Provider
+			value={{
+				token,
+				user,
+				login,
+				logout,
+			}}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => useContext(AuthContext);
