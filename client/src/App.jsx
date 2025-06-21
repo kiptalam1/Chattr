ee070016cab864c14fac78586550a06815a12402
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-21 14:09:37 +0300

    feat: implement a not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 function App() {
 	return (
 		<>
@@ -9,6 +11,7 @@ function App() {
 				<Route path="/" element={<LandingPage />} />
 				<Route path="/auth/login" element={<LoginPage />} />
 				<Route path="/auth/register" element={<RegisterPage />} />
+				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</>
 	);
---
+++ client/src/pages/NotFoundPage.jsx
@@ -0,0 +1,4 @@
+const NotFoundPage = () => (
+	<div className="text-center mt-20 text-red-600">404 - Page Not Found</div>
+);
+export default NotFoundPage;
