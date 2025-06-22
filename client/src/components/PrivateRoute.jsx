e65efc7db40d9d8d34bbf117922f526568b51c84
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-22 12:53:27 +0300

    fix: add is ready for proper auth.

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,8 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import Spinner from "./Spinner";
 
 const PrivateRoute = ({ children }) => {
-	const { token } = useAuth();
+	const { token, isAuthReady } = useAuth();
+
+	if (!isAuthReady) return <Spinner />; // or loading spinner
+
 	return token ? children : <Navigate to="/auth/login" replace />;
 };
 
