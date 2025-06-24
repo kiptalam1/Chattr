2d1bc67981e29f48ea8e44442737270ce67c8210
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-24 19:59:20 +0300

    feat: add functionality to get private messages.

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -16,3 +16,33 @@ export const getMessages = async (req, res) => {
 		res.status(500).json({ success: false, message: "Something went wrong." });
 	}
 };
+
+export const getPrivateMessagesHistory = async (req, res) => {
+	try {
+		const userId = req.user.id;
+
+		if (!userId) {
+			return res
+				.status(401)
+				.json({ success: false, message: "Please login in." });
+		}
+		// Extract targetId from request parameters
+		const { targetId } = req.params;
+
+		const messages = await Message.find({
+			$or: [
+				{ sender: userId, recipient: targetId },
+				{ sender: targetId, recipient: userId },
+			],
+		})
+			.sort({ createdAt: 1 })
+			.populate("sender", "username")
+			.populate("recipient", "username");
+		res.status(200).json({
+			success: true,
+			data: messages,
+		});
+	} catch (error) {
+		res.status(500).json({ success: false, message: "Something went wrong." });
+	}
+};
--- server/routes/message.route.js
+++ server/routes/message.route.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { getMessages } from "../controllers/message.controller.js";
+import {
+	getMessages,
+	getPrivateMessagesHistory,
+} from "../controllers/message.controller.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 // GET /api/v1/messages?roomId=global;
 router.get("/", verifyToken, getMessages);
+// GET /api/v1/messages/private/:targetId;
+router.get("/private/:targetId", verifyToken, getPrivateMessagesHistory);
+    
 
 export default router;
